15deb8aed388b943284252fe0a514a11c865fdfc
Youssef Berrada <yberrada@mit.edu>
2018-10-04 23:14:48 -0400

    add main code

---
+++ src/main.R
@@ -0,0 +1,16 @@
+###                                             ###
+### 15.458 Financial Data Science and Computing ###
+###             FirstName LastName              ###
+###                   email                     ###
+###                 Fall 2018                   ###
+
+
+rm(list=ls())
+options(digit=6)
+# Required Package
+# install.packages("quantmod")
+# install.packages("data.table")
+# install.packages("lubridate")
+library(quantmod)
+library(data.table)
+library(lubridate)
